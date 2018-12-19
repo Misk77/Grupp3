0afaa94263a84fc35734ccdac1a63f98271f9b32
Michel <42797392+Misk77@users.noreply.github.com>
2018-12-19 09:47:56 +0100

    Grupp3-V04

---
+++ src/HostingerMain.java
@@ -0,0 +1,38 @@
+
+public class HostingerMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HostingerMethods HostingerMethods = new HostingerMethods();
+		HostingerMethods.menu(); // avmarkera denna ifall du ska testa metoden nedan
+	//	HostingerMethods.manuallyInsert();  // testar method direkt här
+
+		
+		
+		// END
+	}
+
+}
+
+
+/////////////////////////////////////////////////   VERISON  0.4 \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+//  -- TIPS Om ni vill testa era metoder script så kan det vara smart att ändra table namn så ändrar ni inget ifall andra testar samtidigt, samt så kör metoden direkt i main method så slipper ni menyn :)
+/// Bra att tänka på : man kan bara kopiera lib från nuvarande mysql-java-connector MEN måste oftast rebuild path på nya platsen
+// Denna mall kan använda till vad som helst, böcker, filmer, etc, bara ändra
+// värden och toString(parametrar)
+
+/*
+ * 1. Kolla igenom så inget upprepas, kolla så ingen VARIABLE deklareras och
+ * initieras på olika ställen fast har samma metod 
+ * 2. Snyggare smartare att ha connection metoden kopplat till VARIABLER 
+ * 3. Lägga in querys som kan skrivas
+ * i consolen eller dialogruta med swing eller bara console?? 
+ * 4. Kolla ifall man kan lägga in SQL PROCEDURE ??? DET FINNS, KOLLA UPP HUR
+ * 5. ??
+ *  6. ?? 
+ *  7. ?? 
+ *  8. ??
+ * 
+ * OBS! Kolla stavning, denna mall är gjord i all hast och säkert mycket slarv
+ * stavningar
+ */
